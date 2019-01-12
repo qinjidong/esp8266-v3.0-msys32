12f8d5f697276061b37a8fd322f0a30dec1597d3
sticker <qin1212@vip.qq.com>
2019-01-12 23:50:03 +0800

    esp8266-v3.0-msys32

---
+++ etc/profile.d/esp32_toolchain.sh
@@ -0,0 +1,4 @@
+# This file was created by ESP-IDF windows_install_prerequisites.sh
+# and will be overwritten if that script is run again.
+export PATH="$PATH:/opt/xtensa-lx106-elf/bin"
+export IDF_PATH="/e/ESP/msys32/home/qjy/esp/ESP8266_RTOS_SDK"
